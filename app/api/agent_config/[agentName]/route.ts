2d0171bb5651ed99c096907c331b2985f962c603
abhinavagrahari1 <abhinav.agrahari1@gmail.com>
2025-07-15 12:34:00 +0530

    added async call

--- app/api/agent_config/[agentName]/route.ts
+++ app/api/agent_config/[agentName]/route.ts
@@ -3,7 +3,7 @@ import { API_BASE } from '../../../lib/api';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function GET(req: NextRequest, context: any) {
-  const { agentName } = context.params;
+  const { agentName } = await context.params;
   const res = await fetch(`${API_BASE}/agent_config/${agentName}`);
   const data = await res.json();
   return NextResponse.json(data);
