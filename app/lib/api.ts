8bac12f51e30707c4354b442b7b1e599b35fdd20
abhinavagrahari1 <abhinav.agrahari1@gmail.com>
2025-07-21 17:51:43 +0530

    check

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,6 +1,6 @@
 // export const API_BASE = 'https://13.233.50.22';
 // export const API_BASE = 'http://13.233.50.22:8000'
-export const API_BASE = "https://13.126.59.122"
+export const API_BASE = "http://13.126.59.122"
 // export const API_BASE = 'http://127.0.0.1:8000';
 
 export async function fetchWithErrorHandling<T>(url: string, options: RequestInit = {}): Promise<T> {
